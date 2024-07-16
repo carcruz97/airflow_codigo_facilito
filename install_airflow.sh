7329173c3cf82b63bac1961a6efac00dc2139c54
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-07-16 19:22:59 -0300

    Update install_airflow.sh

--- install_airflow.sh
+++ install_airflow.sh
@@ -1,4 +1,11 @@
-
+#!/bin/bash
+# Step 1: Create a new Conda environment named 'airflow' with Python 3.8
+conda create -n airflow python=3.8 -y
+# Step 2: Activate the 'airflow' environment
+conda activate airflow
+# Step 3: Install necessary packages using pip
+pip3 install scipy pytesseract scikit-image numpy pandas
+# Step 4: Set Airflow version
 AIRFLOW_VERSION=2.9.3
 # Step 5: Extract the version of Python you have installed
 PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
